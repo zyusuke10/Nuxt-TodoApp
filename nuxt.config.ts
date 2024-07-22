fbcbb4acf572870256e9cbe2ea0bc2540a49cccc
yusuke.zamma <yusuke.zamma@rakuten.com>
2024-07-22 15:01:38 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
+  ],
+  typescript: {
+    typeCheck: true
+  }
+})
