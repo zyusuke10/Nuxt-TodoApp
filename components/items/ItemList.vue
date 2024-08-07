aabc843c8892c0f98f49d72eb0aa2c05b5bf40e0
yusuke.zamma <yusuke.zamma@rakuten.com>
2024-08-07 10:04:27 +0900

    Add pagination

--- components/items/ItemList.vue
+++ components/items/ItemList.vue
@@ -1,12 +1,22 @@
 <template>
+  <div>
     <ul class="space-y-3 mt-3">
-      <Item v-for="item in taskList" :key="item.id" :item="item"/>
+      <Item v-for="item in displayTasks" :key="item.id" :item="item"/>
     </ul>
+    <UPagination v-if="taskStore.taskList.length" v-model="currentPage" class="mt-5" :page-count="5" :total="taskStore.taskList.length" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useTaskStore } from '../../store/TaskStore';
 
 const taskStore = useTaskStore()
-const taskList = computed(() => taskStore.taskList);
+const currentPage = ref(1)
+const itemsPerPage = 7
+
+const displayTasks = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  return Array.isArray(taskStore.taskList) ? taskStore.taskList.slice(startIndex, endIndex) : []
+})
 </script>
